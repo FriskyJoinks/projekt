daaf72d47b18d7995de59c540b5d55cdc62761d7
FriskyJoinks <ameen200512@yahoo.com>
2025-01-15 14:53:05 +0100

    Imported lots of things and changed some things

--- index.js
+++ index.js
@@ -1,1 +1,88 @@
 import 'dotenv/config';
+import http from 'http';
+import { ReturnDocument } from 'mongodb';
+import { handleStaticFileRequest } from './static-file-handler.js';
+import { getRequestBody } from './ulities.js';
+
+let websiteTitle = "Gustavo Fring Los Pollos Hermanos";
+let websitedescription = "Hello welocme to los polls hermanos";
+
+async function handleRequest(request, response) {
+    let url = new URL(request.url, 'http://' + request.headers.host);
+    let path = url.pathname;
+    let pathSegments = path.split('/').filter(function (segment) {
+        if (segment === '' || segment === '..') {
+            return false;
+        } else {
+            return true;
+        }
+    });
+
+    let nextSegment = pathSegments.shift();
+
+if (nextSegment=== 'static'){
+    await handleStaticFileRequest(pathSegments, request, response);
+    return;
+}
+
+    if (nextSegment === 'website') {
+        response.writeHead(200, { 'Content-Type': 'text/html;charset=UTF8' });
+        response.write(`
+        <html>
+        <head>
+            <title>Home Page</title>
+
+            <link rel="stylesheet" href="/static/style.css">   
+        </head>
+        <body>
+        <h1>` + cleanupHTMLOutput(websiteTitle) + `</h1>
+        <p>` + cleanupHTMLOutput(websiteDescription) + `</p>
+        </body>
+        </html>
+        `);
+        response.end();
+        return;
+    }
+
+    if (nextSegment === 'form') {
+        response.writeHead(200, { 'Content-Type': 'text/html;charset=UTF8' });
+        response.write(`
+        <html>
+        <head>
+            <title>Home Page</title>
+        </head>
+        <body>
+           <form action ="/change-website-info" method="POST">
+            <input type="text" name="website-name" placeholder="Wesbites name">
+            <textarea name="website-description" placeholder="Beskrivning"></textarea>
+            <button type="submit">Submit</button>
+           </form>
+        </body>
+        </html>
+        `);
+        response.end();
+        return;
+    }
+    if (nextSegment === 'change-website-info') {
+        let body = await getRequestBody(request);
+        let params = new URLSearchParams(body);
+
+        websiteTitle = params.get('website-name');
+        websitedescription = params.get('website-description');
+
+        //response.writeHead(303 , {'Location': '/website'});
+        response.writeHead(200, { 'Content-Type': 'text/plain' })
+        response.write('200 OK');
+        response.end();
+        return;
+    }
+
+    response.writeHead(404, { 'Content-Type': 'text/plain' });
+    response.write('404 Not Found');
+    response.end();
+    return;
+}
+
+let server = http.createServer(handleRequest);
+
+server.listen(process.env.PORT);
---
+++ static-file-handler.js
@@ -0,0 +1,75 @@
+import http from 'http';
+import fs from 'fs/promises';
+import path from 'path';
+
+/**
+ 
+@param {string[]} pathSegments
+@param {http.IncomingMessage} request
+@param {http.ServerResponse} response */
+
+export async function handleStaticFileRequest(pathSegments, request, response) {
+    if (request.method !== 'GET') {
+        response.writeHead(405, {'Content-Type': 'text/plain' });
+        response.write('405 Method Not Allowed');
+        response.end();
+        return;
+    }
+
+
+    let fileName = pathSegments[pathSegments.length - 1];
+    let dotIndex = fileName.lastIndexOf('.');
+
+    if (dotIndex === -1) {
+        response.writeHead(400, { 'Content-Type': 'text/plain' });
+        response.write('400 Bad Request');
+        response.end();
+        return;
+    }
+
+    let ext = fileName.substring(dotIndex + 1);
+
+    let mimeType;
+    if (ext === 'txt'){
+        mimeType = 'text/plain;charset=UTF-8';
+    } else if (ext === 'html'){
+        mimeType = 'text/html;charset=UTF-8';
+    } else if (ext === 'css'){
+        mimeType = 'text/css;charset=UTF-8';
+    } else if (ext === 'png'){
+        mimeType = 'image/png';
+    } else {
+        console.log("HEJ2")
+        response.writeHead(500, { 'Content-Type': 'text/plain' });
+        response.write('500 External Server Error');
+        response.end();
+        return;
+    }
+
+    let filePath = pathSegments.join('/');
+
+
+    let fileContents;
+    try {
+        fileContents = await fs.readFile('public/' + filePath);
+    } catch(err) {
+        if (err.code === 'ENOENT'){ // ENOENT 
+            response.writeHead(200, { 'Content-Type': 'text/plain' });
+            response.write('404 Not Found');
+            response.end();
+            return;
+        } else {
+            response.writeHead(500, { 'Content-Type': 'text/plain' });
+            response.write('500 Internal Server Error');
+            response.end();
+            return;
+        }
+    }
+
+
+    response.writeHead(200, { 'Content-Type': mimeType });
+    response.write(fileContents);
+    response.end();
+    // index.html
+
+}
